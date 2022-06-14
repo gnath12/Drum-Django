6b73cb5b1c2a51be4a9d277cbc57fad63f4ff693
dlworn5434 <91576405+dlworn5434@users.noreply.github.com>
2022-06-14 16:49:22 +0900

    로그인 로그아웃
    로그인 로그아웃

--- instagram/settings.py
+++ instagram/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instagram',
-    'content'
+    'content',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+AUTH_USER_MODEL = 'user.User'
